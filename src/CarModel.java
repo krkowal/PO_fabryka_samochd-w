1d623a93a4ad727e466ed1e930aab0b2e0832ad9
kowal <rogalix1308@gmail.com>
2021-12-16 19:26:09 +0100

    end commit

--- src/CarModel.java
+++ src/CarModel.java
@@ -1,3 +1,8 @@
 public enum CarModel {
-    modelA,modelB;
+    modelA(10000),
+    modelB(20000);
+    public int cost;
+    CarModel(int cost) {
+        this.cost=cost;
+    }
 }
--- src/EngineCapacity.java
+++ src/EngineCapacity.java
@@ -3,8 +3,14 @@
 
 public enum EngineCapacity {
     
-    L(1.2);
+    A(1.0),
+    B(1.2),
+    C(1.4),
+    D(1.6);
+
+    public double capacity;
 
     EngineCapacity(double v) {
+        this.capacity=v;
     }
 }
